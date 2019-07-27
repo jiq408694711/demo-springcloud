7c30ffe5b1f2fcac944690dbc8c93aa52464639b
yiqin.ji <jiq408694711@163.com>
2019-07-28 01:05:44 +0800

    profile配置文件

--- demo-common/src/main/java/com/xx/sbc/web1/facade/DemoServiceFacade.java
+++ demo-common/src/main/java/com/xx/sbc/web1/facade/DemoServiceFacade.java
@@ -2,8 +2,7 @@ package com.xx.sbc.web1.facade;
 
 import com.xx.sbc.web1.response.HelloResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -12,6 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "demo-service", path = "web1/module1", fallback = DemoServiceFacadeFallback.class)
 public interface DemoServiceFacade {
 
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @GetMapping(value = "/get")
     HelloResponse get(@RequestParam("id") Long id);
 }
