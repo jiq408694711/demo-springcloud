a4173bc3392a30640c9908eac2fd28dd9ff97258
yiqin.ji <jiq408694711@163.com>
2019-08-05 00:33:18 +0800

    dubbo

--- demo-dubbo-service/src/main/java/com/xx/sbc/DemoDubboServiceApplication.java
+++ demo-dubbo-service/src/main/java/com/xx/sbc/DemoDubboServiceApplication.java
@@ -1,13 +1,15 @@
 package com.xx.sbc;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DemoDubboServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoDubboServiceApplication.class, args);
+        new SpringApplicationBuilder(DemoDubboServiceApplication.class)
+                .web(WebApplicationType.NONE) // NONE为非web工程，一般用于专门提供接口的jar
+                .run(args);
     }
-
 }
